7c877fb7ed817983e89424da2135f4e8310766e0
Bulat <bulatkazan@yahoo.com>
2023-10-02 20:44:38 +0600

    feat: add input form

---
+++ src/app/task-form/task-form.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-task-form',
+  templateUrl: './task-form.component.html',
+  styleUrls: ['./task-form.component.css']
+})
+export class TaskFormComponent {
+  task = "Test task";
+
+  submitted = false;
+
+  onSubmit() { this.submitted = true; }
+}
